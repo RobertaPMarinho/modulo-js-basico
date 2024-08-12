dd43f57a9fec6466d6179fc0b765a5d9d5563b0d
Roberta Pereira Marinho <robertapmarinho@gmail.com>
2024-08-12 16:32:48 -0300

    adicionado aula 03-variáveis

---
+++ 03-variaveis/index.js
@@ -0,0 +1,11 @@
+{
+  var   nomePessoa = "Roberta"
+
+  const emailPessoa = "robertapmarinho@gmail.com"
+
+  let telefonePessoa = "51983004515"
+}
+
+console.log(nomePessoa)
+console.log(emailPessoa)
+console.log(telefonePessoa)
