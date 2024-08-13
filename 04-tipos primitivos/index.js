22e8f47458fcaafa9a9ed5208b4f8860f94bedb7
Roberta Pereira Marinho <robertapmarinho@gmail.com>
2024-08-13 14:48:44 -0300

    adicionado 04-tipos primitivos

---
+++ 04-tipos primitivos/index.js
@@ -0,0 +1,16 @@
+let nome = "Roberta"
+
+let numero = 12.6
+
+let usuarioVerificado = false
+
+if(usuarioVerificado) {
+  console.log("usuário verificado")
+} else {
+  console.log("usuário não  é verificado")
+}
+
+let pessoa = null
+
+let telefone = undefined
+console.log(telefone)
