fb649315611e3de9908d7a125d79526aba974de1
Roberta Pereira Marinho <robertapmarinho@gmail.com>
2024-08-13 21:12:57 -0300

    adicionado 05-operadores lógicos

---
+++ 05-operadores-logicos/index.js
@@ -0,0 +1,15 @@
+/*
+== ---igual--- a==b --- verdadeiro se a for igual a b
+=== ---idêntico--- a===b --- verdadeiro se a for idêntico a b
+!= ---diferente--- a!=b --- verdadeiro se o a for diferente de b
+!== ---não idêntico--- a!==b --- verdadeiro se o a não for idêntico a b
+< ---menor que--- a < b --- verdadeiro quando a for menor que b
+<= ---menor igual que--- a <= b --- verdadeiro quando a for menor ou igual a b
+> ---maior que--- a > b --- verdadeiro quando a for maior que b
+>= ---maior igual que--- a >= b --- verdadeiro quando a for maior ou igual a b
+*/ 
+
+const a = 3
+const b = 10
+
+console.log(a >= b)
