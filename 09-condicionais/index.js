cea07e0c14d9f8af636cb2593929db6d6f0f7301
Roberta Pereira Marinho <robertapmarinho@gmail.com>
2024-08-16 14:57:26 -0300

    09-condicionais - IF/ELSE

---
+++ 09-condicionais/index.js
@@ -0,0 +1,16 @@
+let nomeFilme = 'Vingadores'
+/*
+if(nomeFilme === 'Vingadores') {
+  console.log('É o filme dos Vingadores')
+}else if(nomeFilme === 'Batman vs Superman') {
+  console.log('É o filme do Batman vs Superman')
+}else {
+  console.log('É outro filme')
+}
+*/
+
+//Operador ternário
+
+false ? console.log('condição verdadeira') : console.log('condição falsa')
+
+nomeFilme === 'Vingadores' ? console.log('É o filme dos Vingadores') : console.log('É outro filme')
